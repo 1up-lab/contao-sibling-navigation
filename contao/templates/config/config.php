1fdad0fe2e22fd80524e89cc8a9242b19daecc54
David Greminger <david.greminger@1up.io>
2015-01-08 10:40:04 +0100

    Removed wrong duplicates

--- contao/templates/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$GLOBALS['TL_CTE']['links'] = array_merge($GLOBALS['TL_CTE']['links'], array(
-    'oneup_sibling_navigation' => 'SiblingNavigation',
-));
--- contao/templates/dca/tl_content.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$GLOBALS['TL_DCA']['tl_content']['palettes']['oneup_sibling_navigation'] = '{type_legend},type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
