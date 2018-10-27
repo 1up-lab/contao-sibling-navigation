4b5e7cc3c5e3802366a503c3ab98546a687467af
David Greminger <david.greminger@1up.io>
2018-10-27 16:08:36 +0200

    Fix E_WARNING, see #4

--- contao/config/config.php
+++ contao/config/config.php
@@ -6,13 +6,18 @@ $GLOBALS['TL_CTE']['links'] = array_merge($GLOBALS['TL_CTE']['links'], [
     'sibling_navigation_page'  => 'Oneup\SiblingNavigation\ContentElement\Page\SiblingNavigation',
 ]);
 
-$GLOBALS['FE_MOD']['news'] = array_merge($GLOBALS['FE_MOD']['news'], [
-    'sibling_navigation_news'  => 'Oneup\SiblingNavigation\Module\News\SiblingNavigation',
-]);
+if (array_key_exists('news', $GLOBALS['FE_MOD'])) {
+    $GLOBALS['FE_MOD']['news'] = array_merge($GLOBALS['FE_MOD']['news'], [
+        'sibling_navigation_news'  => 'Oneup\SiblingNavigation\Module\News\SiblingNavigation',
+    ]);
+}
 
-$GLOBALS['FE_MOD']['events'] = array_merge($GLOBALS['FE_MOD']['events'], [
-    'sibling_navigation_event' => 'Oneup\SiblingNavigation\Module\Event\SiblingNavigation',
-]);
+
+if (array_key_exists('events', $GLOBALS['FE_MOD'])) {
+    $GLOBALS['FE_MOD']['events'] = array_merge($GLOBALS['FE_MOD']['events'], [
+        'sibling_navigation_event' => 'Oneup\SiblingNavigation\Module\Event\SiblingNavigation',
+    ]);
+}
 
 $GLOBALS['FE_MOD']['navigationMenu'] = array_merge($GLOBALS['FE_MOD']['navigationMenu'], [
     'sibling_navigation_page'  => 'Oneup\SiblingNavigation\Module\Page\SiblingNavigation',
